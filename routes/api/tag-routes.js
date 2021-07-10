6feefe9ead6bdcef0139176047358aa50bd9bb26
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-07-10 11:06:27 -0500

    wrote function for deleting tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,8 +35,14 @@ router.put('/:id', (req, res) => {
   })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-});
+  const deleteTag = await Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  return res.json(deleteTag)
+})
 
 module.exports = router;
