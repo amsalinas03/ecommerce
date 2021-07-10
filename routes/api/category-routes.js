6b39a3320f38adf07a6fc57977fa15b210d24a3c
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-07-10 11:09:18 -0500

    wrote functions for creating new category
    and update a category's id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,10 +23,16 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.updateupdate(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 router.delete('/:id', (req, res) => {
